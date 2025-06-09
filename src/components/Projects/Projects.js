725a214bf6a08044206c35f8e6408f3f502a8b79
lourimi <ayoub.lourimi@issatso.u-sousse.tn>
2025-06-10 00:27:05 +0100

    Update Projects.js

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -36,7 +36,7 @@ function Projects() {
               title="Portfolio"
               description="Explore my personalized portfolio crafted with React JS, showcasing my projects, skills, and experiences. Engage with dynamic design, seamless navigation, and interactive elements, reflecting my dedication to innovation and creativity in the digital realm."
               ghLink="https://github.com/lourimi-ayoub/Portfolio"
-              demoLink="https://.vercel.app/"
+              demoLink="https://lourimi-portfolio.vercel.app/"
             />
           </Col>
 
